0eae0f2dec4feeac1c4ec16da7095903bb4ba075
David <david.crittin@gmail.com>
2020-12-18 10:40:13 +0100

    Changed using

--- VSFlyWebAPI/Controllers/FlightsController.cs
+++ VSFlyWebAPI/Controllers/FlightsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using EFCoreApp2020E;
+using VSFlyEFCoreApp;
 
 namespace VSFlyWebAPI.Controllers
 {
